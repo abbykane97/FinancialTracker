21e0ef4aaa80f8dcdfc3f5c97ea57cea3b75226d
abbkane97 <kanehaby20@gmail.com>
2024-04-29 15:12:35 -0400

    Created the home screen feature

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Transaction {
+    public static void main(String [] args) {
+        FinancialTracker financialTracker = new FinancialTracker();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Your Financial Tracker App !");
+
+        while (true) {
+            System.out.println("\nHome Screen:");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make a Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+
+            System.out.print("Please select an option: ");
+            String choice = scanner.next().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    financialTracker.addDeposit(scanner);
+                    break;
+                case "P":
+                    financialTracker.makePayment(scanner);
+                    break;
+                case "L":
+                    financialTracker.displayLedger();
+                    break;
+                case "X":
+                    System.out.println("Exiting Financial Tracker App. Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
 }
+
+
+
+
